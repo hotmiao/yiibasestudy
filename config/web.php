01f9cd08d5aa98aae414f99b350a3c6245ec3629
Miao Dawei <work_style@163.com>
2018-04-08 21:30:26 +0800

    'elasticsearch'

--- config/web.php
+++ config/web.php
@@ -12,6 +12,13 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'elasticsearch' => [
+            'class' => 'yii\elasticsearch\Connection',
+            'nodes' => [
+                ['http_address' => '127.0.0.1:9200'],
+                // configure more hosts if you have a cluster
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'izVE7s9fznNVmFT4654RxRXNUxDfQfhy',
